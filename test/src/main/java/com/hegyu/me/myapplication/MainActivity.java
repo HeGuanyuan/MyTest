a05ddbee332b104cca969473662d6a5933c329c1
HeGuanyuan <master@heguanyuan.com>
2016-03-11 22:39:41 +0800

    Initial commit

---
+++ test/src/main/java/com/hegyu/me/myapplication/MainActivity.java
@@ -0,0 +1,30 @@
+package com.hegyu.me.myapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        assert fab != null;
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "添加你自己的操作", Snackbar.LENGTH_SHORT)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
